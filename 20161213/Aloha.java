bdcc14215219e70dc023b05a5c99c09e2c0db831
hasi10 <hasimoto_shota@yse-c.net>
2016-12-13 15:28:19 +0900

    Aloha.java added.

---
+++ 20161213/Aloha.java
@@ -0,0 +1,15 @@
+public class Aloha {
+	public static void main(String[] args) {
+		String mainMsg;
+		String subMsg;
+
+	mainMsg = new java.util.Scanner(System.in).nextLine();
+	subMsg = new java.util.Scanner(System.in).nextLine();
+
+	System.out.println(mainMsg);
+	System.out.println(subMsg);
+
+			}
+		}
+	}
+}
