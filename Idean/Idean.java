8a5c7e85a2aab6ae649bbe6fb57911a2f6df81c8
hasi10 <160046@yses.ac.jp>
2017-01-23 11:58:46 +0900

    up.

--- Idean/Idean.java
+++ Idean/Idean.java
@@ -1,4 +1,4 @@
-import java.awt.*
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
@@ -6,5 +6,27 @@ import java.io.*;
 public class Idean {
 	public static void main(String[] args) {
 		IdeanMan im = new IdeanMan();
+	}
+}
+
+
+class IdeanMan implements ActionListener{
+	JFrame frame;
+	JButton btn;
+	JPanel panel;
+	JTextArea area;
+	JScrollPane scrollPAne;
+	String filedata;
+	String textdata;
+
+	public IdeanMan() {
+		frame = new JFrame("Idean");
+		frame.setLocation(400,300);
+		frame.setSize(500,400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public void actionPerformed(ActionEvent ae){
+
 	}
 }
