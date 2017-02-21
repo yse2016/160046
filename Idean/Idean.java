3f7eff5ac61242a36d62c732ab4f1b446e3db298
hasi10 <160046@yses.ac.jp>
2017-02-21 09:04:40 +0900

    up.

--- Idean/Idean.java
+++ Idean/Idean.java
@@ -9,6 +9,7 @@ public class Idean {
     }
 }
 
+<<<<<<< HEAD
 
 class IdeanMan implements ActionListener {
     // field
@@ -82,6 +83,75 @@ class IdeanMan implements ActionListener {
         Container con = frame.getContentPane();
         con.setLayout(new GridLayout(3, 1));
         con.add(worldsPanel);
+=======
+class IdeanMan implements ActionListener{
+    JFrame frame;
+    JButton btnOpen;
+    JButton btnSave;
+    JTextArea area;
+    JTextArea area2;
+    JTextArea textArea;    
+    JPanel btnPanel;
+    JPanel panel;
+    JLabel word1;
+    JLabel word2;
+    JPanel wordsPanel;
+    JTextField field;
+    JScrollPane scrollPane;
+    String filedata;
+    String textdata;
+    int num;
+
+    
+    public IdeanMan(){
+        frame = new JFrame("Idean");
+        frame.setLocation(400,400);
+        frame.setSize(450,450);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        btnOpen = new JButton("Open");
+        btnOpen.addActionListener(this);
+        btnOpen.setActionCommand("open");  
+
+        btnSave = new JButton("Save");
+        btnSave.addActionListener(this);
+        btnSave.setActionCommand("save");
+
+        btnPanel = new JPanel();
+        btnPanel.add(btnOpen);
+        btnPanel.add(btnSave);
+
+        field = new JTextField(20);
+
+        word1 = new JLabel("WORD1");
+        word2 = new JLabel("WORD2");
+        wordsPanel = new JPanel();
+        wordsPanel.add(word1);
+        wordsPanel.add(word2);
+        word1.setText("ALOHA");
+        word2.setText("Good Afternoon");
+
+        area = new JTextArea(10,30);
+        area2 = new JTextArea(5,20);
+        area.setLineWrap(true);
+
+        textArea = new JTextArea(5, 20);
+
+        scrollPane = new JScrollPane(textArea);
+
+        panel = new JPanel();
+        panel.add(field);
+        panel.add(btnOpen);
+        panel.add(btnSave);
+        panel.add(area2);
+
+        num = (int)(Math.random()*10);
+
+        Container con = frame.getContentPane();
+        con.setLayout(new GridLayout(2,1));
+        con.add(panel);
+        con.add(wordsPanel);
+>>>>>>> createjs
         con.add(btnPanel);
         con.add(scrollPane);
 
@@ -127,6 +197,17 @@ public void actionPerformed(ActionEvent ae) {
             if (ret == JFileChooser.APPROVE_OPTION) {
                 // 選ばれたファイル
                 File file = fc.getSelectedFile();
+<<<<<<< HEAD
+=======
+                filedata = file.getAbsolutePath();
+                field.setText(filedata);
+            }
+
+            try {
+                File inFile                 = new File(filedata);
+                FileReader fr               = new FileReader(inFile);
+                BufferedReader br           = new BufferedReader(fr);
+>>>>>>> createjs
 
                 // ファイル名、保存場所を調べる
                 textFileName = file.getAbsolutePath();
